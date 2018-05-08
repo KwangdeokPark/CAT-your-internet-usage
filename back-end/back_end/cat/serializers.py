be19fa74e42b8765dee73916c5820097088838eb
pkd <pkd@pkd-MacBook-Pro.local>
2018-05-08 17:35:09 +0900

    define CatUserSerializer

---
+++ back-end/back_end/cat/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from cat.models import CatUser, Group, Timeline, Setting, Join
+
+class CatUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CatUser
+        fields = ('id',
+                  'username',
+                  'password',
+                  'age',
+                  'today_spent_time',
+                  'now_spent_time',
+                  'last_record_date',)
+
+
