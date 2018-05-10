b0deef377a1fef4233d7398d70c09944b234ab48
ialy1595 <ialy12006@gmail.com>
2018-05-10 18:22:05 +0900

    test view now time

--- front-end/client/routes.js
+++ front-end/client/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import Greetings from './components/Greetings';
 import SignupPage from './components/signup/SignupPage';
+import NowTime from './components/NowTime';
 
 export default (
   <Route path="/" component = {App} >
     <IndexRoute component={Greetings}/>
+    <Route path ="nowtime" component={NowTime}/>
     <Route path ="signup" component={SignupPage}/>
   </Route>
 )
