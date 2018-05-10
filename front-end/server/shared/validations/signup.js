151ba70c1440ce62ff9e4756224bcebd84ff92a1
jueun <je2156@hanmail.net>
2018-05-10 23:37:45 +0900

    login form

--- front-end/server/shared/validations/signup.js
+++ front-end/server/shared/validations/signup.js
@@ -7,23 +7,23 @@ export default function validateInput(data){
   if(Validator.isNull(data.username)){
     errors.username='This field is required';
   }
-  if(Validator.isNull(data.password)){
-    errors.password='This field is required';
+  if(Validator.isNull(data.password1)){
+    errors.password1='This field is required';
   }
-  if(Validator.isNull(data.passwordConfirmation)){
-    errors.passwordConfirmation='This field is required';
+  if(Validator.isNull(data.password2)){
+    errors.password2='This field is required';
   }
-  if(!Validator.equals(data.password, data.passwordConfirmation)){
-    errors.passwordConfirmation = 'Passwords must match';
+  if(!Validator.equals(data.password1, data.password2)){
+    errors.password2 = 'Passwords must match';
   }
   if(Validator.isNull(data.age)){
     errors.age='This field is required';
   }
-  if(Validator.isNull(data.alertStartTime)){
-    errors.alertStartTime='This field is required';
+  if(Validator.isNull(data.alert_start_time)){
+    errors.alert_start_time='This field is required';
   }
-  if(Validator.isNull(data.alertInterval)){
-    errors.alertInterval='This field is required';
+  if(Validator.isNull(data.alert_interval)){
+    errors.alert_interval='This field is required';
   }
 
   return {
