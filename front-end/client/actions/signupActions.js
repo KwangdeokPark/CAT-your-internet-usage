34864f8f8c5b9dde046ed16f915d2e0a014bc110
pkd <pkd@pkd-MacBook-Pro.local>
2018-05-11 19:36:14 +0900

    remove front_end

--- front-end/client/actions/signupActions.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export function userSignupRequest(userData){
-  return dispatch => {
-    return axios.post('http://127.0.0.1:8000/sign_up/', userData);
-    //.catch(exc=>console.log(exc));
-  }
-}
