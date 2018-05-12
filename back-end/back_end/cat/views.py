7f8a8184846ef59bcaddd4ffbb4847c922673522
pkd <pkd@pkd-MacBook-Pro.local>
2018-05-12 14:14:34 +0900

    end testing sigin up

--- back-end/back_end/cat/views.py
+++ back-end/back_end/cat/views.py
@@ -5,17 +5,6 @@ from django.contrib.auth.models import User
 from rest_framework.exceptions import ValidationError
 from datetime import datetime, timedelta
 
-'''
-class SingUp(generics.ListCreateAPIView):
-    queryset = CatUser.objects.all()
-    serializer_class = CatUserSerializer
-
-
-class SignIn(generics.ListAPIView):
-    queryset = CatUser.objects.all()
-    serializer_class = CatUserSerializer
-'''
-
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 import datetime
@@ -27,7 +16,6 @@ from django.views.generic.base import TemplateView
 
 from django.http import HttpResponse
 
-# from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 
@@ -35,8 +23,6 @@ from rest_framework.decorators import api_view
 class mainview(TemplateView):
     template_name = 'main.html'
 
-
-# @ensure_csrf_cookie
 @api_view(['POST'])
 @csrf_exempt
 def signup(request):
@@ -46,11 +32,6 @@ def signup(request):
         print(form)
         if form.is_valid():
             user = form.save()
-            '''
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user1 = authenticate(username=username, password=raw_password)
-            '''
             user.catuser.age = form.cleaned_data.get('age')
             print(user.catuser.age)
             user.catuser.today_spent_time = datetime.timedelta(minutes=0)
@@ -71,21 +52,6 @@ def signup(request):
                 alert_interval=datetime.timedelta(minutes=form.cleaned_data.get('alert_interval')))
             print(user.catuser.setting)
             user.catuser.save()
-            '''
-            age = form.cleaned_data.get('age')
-            alert_start_time = form.cleaned_data.get('alert_start_time')
-            alert_interval = form.cleaned_data.get('alert_interval')
-            catuser = CatUser.objects.create(user=user,
-                                             age=age,
-                                             today_spent_time=datetime.timedelta(minutes=0),
-                                             now_start_time=timezone.now(),
-                                             last_record_time=timezone.now(),
-                                             timeline=Timeline.objects.create(sun_average=timedelta(), mon_average=timedelta(), tue_average=timedelta(), wed_average=timedelta(), thu_average=timedelta(), fri_average=timedelta(), sat_average=timedelta(),
-                                                                              sun_count=0, mon_count=0, tue_count=0, wed_count=0, thu_count=0, fri_count=0, sat_count=0),
-                                             setting=Setting.objects.create(alert_start_time=datetime.timedelta(hours=alert_start_time), alert_interval=datetime.timedelta(minutes=alert_interval)))
-
-            catuser.save()
-            '''
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user1 = authenticate(username=username, password=raw_password)
@@ -94,7 +60,8 @@ def signup(request):
             return redirect('signin')
     else:
         form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+    return redirect('signin')
+    #return render(request, 'signup.html', {'form': form})
 
 
 @api_view(['POST'])
@@ -104,16 +71,18 @@ def signin(request):
         print(request.data)
         form = SignInForm(request.data)
         print(form)
-        username = request.data['identifier']
+        username = request.data['username']
         password = request.data['password']
         user = authenticate(username=username, password=password)
         print(user)
-        print(CatUser.objects.get(user=User.objects.get(username=username)))
+
         if user is not None:
+            print(CatUser.objects.get(user=User.objects.get(username=username)).age)
             login(request, user)
             return redirect('main')
         else:
-            return HttpResponse('로그인 실패. 다시 시도 해보세요.')
+            return redirect('signin')
+            #return HttpResponse('로그인 실패. 다시 시도 해보세요.')
     else:
         form = SignInForm()
         return render(request, 'signin.html', {'form': form})
