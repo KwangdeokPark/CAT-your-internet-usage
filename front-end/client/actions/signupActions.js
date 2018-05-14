3d6ce5f90adeac870a966009c5255c69840ae1d3
jueun <je2156@hanmail.net>
2018-05-14 17:18:53 +0900

    api

--- front-end/client/actions/signupActions.js
+++ front-end/client/actions/signupActions.js
@@ -2,6 +2,5 @@ import axios from 'axios';
 
 export function userSignupRequest(userData){
   return dispatch => {
-    return axios.post('http://127.0.0.1:8000/sign_up/', userData).catch(exc=>console.log(exc));
-  }
+    return axios.post('/api/sign_up/', userData).catch(exc=>console.log(exc));  }
 }
