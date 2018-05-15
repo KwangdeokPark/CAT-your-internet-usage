46d0c9c76e68e3d0a7352ff1e4da46e3de6247a2
jueun <je2156@hanmail.net>
2018-05-15 12:29:28 +0900

    login success message

--- front-end/client/components/login/LoginForm.js
+++ front-end/client/components/login/LoginForm.js
@@ -3,6 +3,7 @@ import TextFieldGroup from '../common/TextFieldGroup';
 import validateInput from '../../../server/shared/validations/login';
 import { connect } from 'react-redux';
 import { login } from '../../actions/authActions';
+import { addFlashMessage } from '../../actions/flashMessages';
 
 class LoginForm extends React.Component {
   constructor(props) {
@@ -33,8 +34,14 @@ class LoginForm extends React.Component {
     if (this.isValid()) {
       this.setState({ errors: {}, isLoading: true });
       this.props.login(this.state).then(
-        (res) => this.context.router.push('/'),
-        (err) => this.setState({ errors: err.response.data.errors, isLoading: false })
+        () => {
+          this.props.addFlashMessage({
+            type: 'success',
+            text: 'You signed in successfully. Hi!'
+          });
+          this.context.router.push('/');
+        },
+        (err) => this.setState({ errors: err.response.data, isLoading: false })
       );
     }
   }
@@ -76,11 +83,12 @@ class LoginForm extends React.Component {
 }
 
 LoginForm.propTypes = {
-  login: React.PropTypes.func.isRequired
+  login: React.PropTypes.func.isRequired,
+  addFlashMessage: React.PropTypes.func.isRequired
 }
 
 LoginForm.contextTypes = {
   router: React.PropTypes.object.isRequired
 }
 
-export default connect(null, { login })(LoginForm);
+export default connect(null, { login, addFlashMessage })(LoginForm);
